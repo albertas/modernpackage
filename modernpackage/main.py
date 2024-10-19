2d0527f0443472ec20bd9518abd79a8c079a8216
Albertas Gimbutas <albertasgim@gmail.com>
2024-10-19 17:54:31 +0300

    Show package version if -v or --version is provided

--- modernpackage/main.py
+++ modernpackage/main.py
@@ -1,6 +1,17 @@
 """Example package configuration using bleeding edge toolset."""
 
+import argparse
+
 
 def main() -> str:
-    """Return 'Hello world'."""
-    return 'Hello world'
+    """Return 'Hello world!' or package version if -v option is provided."""
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--version', help='Show package version.',
+                        action='store_true', default=False)
+    parsed_args = parser.parse_args()
+    if parsed_args.version:
+        from modernpackage import __version__
+        return f"modernpackage {__version__}"
+
+    return "Hello world!"
