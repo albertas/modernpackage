9a1ab39bdd6275a48ecaa44e048bc91a9240b636
Albertas Gimbutas <albertasgim@gmail.com>
2024-10-20 09:40:43 +0300

    Fix some linting errors

--- modernpackage/main.py
+++ modernpackage/main.py
@@ -1,40 +1,53 @@
 """Example package configuration using bleeding edge toolset."""
 
-from argparse import ArgumentParser, Namespace
-from modernpackage import __version__
-from subprocess import Popen, PIPE
 import os
+from argparse import ArgumentParser, Namespace
+from subprocess import PIPE, Popen
+
+from modernpackage import __version__
 
 
 def parse_args() -> Namespace:
+    """Parse CLI options and return them as Namespace (object instance)."""
     parser = ArgumentParser()
-    parser.add_argument('-v', '--version', help='Show package version.',
-                        action='store_true', default=False)
-    parser.add_argument('package_name', help='Name of a new pacakge to initialise in a local directory.', nargs='?')
-    parsed_args = parser.parse_args()
-    return parsed_args
+    parser.add_argument(
+        '-v',
+        '--version',
+        help='Show package version.',
+        action='store_true',
+        default=False,
+    )
+    parser.add_argument(
+        'package_name',
+        help='Name of a new pacakge to initialise in a local directory.',
+        nargs='?',
+    )
+    return parser.parse_args()
 
 
 def init_new_package(package_name: str) -> None:
-    """Clone modernpackage files to `package_name` and run `make init` in it."""
+    """Clone modernpackage files into `package_name` and run `make init` in it."""
     new_package_path = os.path.join(os.getcwd(), package_name)
 
-    print("Cloning modernpackage files to", new_package_path)
-    pipe = Popen(["git", "clone", "https://github.com/albertas/modernpackage", new_package_path], stdin=PIPE, stdout=PIPE)
+    pipe = Popen(
+        ['git', 'clone', 'https://github.com/albertas/modernpackage', new_package_path],
+        stdin=PIPE,
+        stdout=PIPE,
+    )
     pipe.communicate()[0]
 
-    pipe = Popen(["make", "init", package_name], stdin=PIPE, stdout=PIPE, cwd=new_package_path)
-    output = pipe.communicate()[0].decode().split("make:")[0].strip()
-    print(output)
+    pipe = Popen(
+        ['make', 'init', package_name], stdin=PIPE, stdout=PIPE, cwd=new_package_path
+    )
+    pipe.communicate()[0].decode().split('make:')[0].strip()
 
 
 def main() -> None:
     """Return 'Hello world!' or package version if -v option is provided."""
-
     parsed_args = parse_args()
 
     if parsed_args.version:
-        print(f"modernpackage {__version__}")
+        print(f"modernpackage {__version__}")  # noqa: T201
 
     elif parsed_args.package_name:
         init_new_package(package_name=parsed_args.package_name)
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,8 +5,10 @@ from modernpackage.main import main
 
 
 def test_show_version() -> None:
-    with (patch("modernpackage.main.ArgumentParser") as argparse_mock,
-          patch("modernpackage.main.print") as print_mock):
+    with (
+        patch('modernpackage.main.ArgumentParser') as argparse_mock,
+        patch('modernpackage.main.print') as print_mock,
+    ):
         argparse_mock().parse_args().version = True
         main()
-        print_mock.assert_called_once_with(f"modernpackage {__version__}")
+        print_mock.assert_called_once_with(f'modernpackage {__version__}')
