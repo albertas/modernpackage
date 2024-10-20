0f16fe24ff05992302c3a7f5972cbbb1ff2cad62
Albertas Gimbutas <albertasgim@gmail.com>
2024-10-20 09:23:56 +0300

    Improve test to check if `-v` cli option is working

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,12 @@
+from unittest.mock import patch
+
+from modernpackage import __version__
 from modernpackage.main import main
 
 
-class TestMain:
-    def test_main_success(self) -> None:
-        assert main() == 'Hello world'
+def test_show_version() -> None:
+    with (patch("modernpackage.main.ArgumentParser") as argparse_mock,
+          patch("modernpackage.main.print") as print_mock):
+        argparse_mock().parse_args().version = True
+        main()
+        print_mock.assert_called_once_with(f"modernpackage {__version__}")
