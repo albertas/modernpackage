c9a538f871c82eaa450b079ec6309db2d279dbe9
Albertas Gimbutas <albertasgim@gmail.com>
2024-10-20 14:23:04 +0300

    Add validation for package_name to ensure it is alpha numeric
    Also, fix all remaining Ruff linting errors.

--- modernpackage/main.py
+++ modernpackage/main.py
@@ -1,10 +1,18 @@
 """Example package configuration using bleeding edge toolset."""
 
-import os
-from argparse import ArgumentParser, Namespace
+from argparse import ArgumentParser, ArgumentTypeError, Namespace
+from pathlib import Path
 from subprocess import PIPE, Popen
 
 from modernpackage import __version__
+
+
+def check_alpha_numeric(value: str) -> str:
+    """Validate value to contain only Letters and Numbers."""
+    if not value.isalnum():
+        message = 'Non-AlphaNumeric package name'
+        raise ArgumentTypeError(message)
+    return value
 
 
 def parse_args() -> Namespace:
@@ -21,23 +29,24 @@ def parse_args() -> Namespace:
         'package_name',
         help='Name of a new pacakge to initialise in a local directory.',
         nargs='?',
+        type=check_alpha_numeric
     )
     return parser.parse_args()
 
 
 def init_new_package(package_name: str) -> None:
     """Clone modernpackage files into `package_name` and run `make init` in it."""
-    new_package_path = os.path.join(os.getcwd(), package_name)
+    new_package_path = Path.cwd() / package_name
 
-    pipe = Popen(
-        ['git', 'clone', 'https://github.com/albertas/modernpackage', new_package_path],
+    pipe = Popen(  # noqa: S603
+        ['git', 'clone', 'https://github.com/albertas/modernpackage', new_package_path],  # noqa: S607
         stdin=PIPE,
         stdout=PIPE,
     )
     pipe.communicate()[0]
 
-    pipe = Popen(
-        ['make', 'init', package_name], stdin=PIPE, stdout=PIPE, cwd=new_package_path
+    pipe = Popen(  # noqa: S603
+        ['make', 'init', package_name], stdin=PIPE, stdout=PIPE, cwd=new_package_path  # noqa: S607
     )
     pipe.communicate()[0].decode().split('make:')[0].strip()
 
@@ -47,7 +56,7 @@ def main() -> None:
     parsed_args = parse_args()
 
     if parsed_args.version:
-        print(f"modernpackage {__version__}")  # noqa: T201
+        print(f'modernpackage {__version__}')  # noqa: T201
 
     elif parsed_args.package_name:
         init_new_package(package_name=parsed_args.package_name)
